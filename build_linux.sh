fa271811c26a832563716076ee9570a8bcb31775
Vasiliy Tereshkov <52492460+vtereshkov@users.noreply.github.com>
2020-05-06 12:40:37 +0300

    Update Linux build script

--- build_linux.sh
+++ build_linux.sh
@@ -2,10 +2,10 @@
 cd src
 
 gcc -fPIC -O3 -Wall -Wno-format-security -c umka_api.c umka_common.c umka_compiler.c umka_const.c umka_decl.c umka_expr.c umka_gen.c umka_ident.c umka_lexer.c umka_runtime.c umka_stmt.c umka_types.c umka_vm.c 
-gcc -shared -fPIC -static-libgcc *.o -o libumka.so -lm -lumka 
+gcc -shared -fPIC -static-libgcc *.o -o libumka.so -lm 
 
 gcc -O3 -Wall -Wno-format-security -c umka.c 
-gcc umka.o -o umka -static-libgcc -lm -lumka -Wl,-rpath,'$ORIGIN'
+gcc umka.o -o umka -static-libgcc -L$PWD -lm -lumka -Wl,-rpath,'$ORIGIN'
 
 rm -f *.o
 cd ..
