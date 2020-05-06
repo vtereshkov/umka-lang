04cf6659f8aeb6984ef4bd2f5f758f2fb50975c5
Vasiliy Tereshkov <52492460+vtereshkov@users.noreply.github.com>
2020-05-06 03:39:28 +0300

    Add stack/storage size command-line arguments

--- src/umka.c
+++ src/umka.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "umka_api.h"
 
@@ -17,14 +18,54 @@ void meow(UmkaStackSlot *params, UmkaStackSlot *result)
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
+    if (argc < 2)
     {
         printf("Umka interpreter (C) Vasiliy Tereshkov, 2020\n");
-        printf("Usage: umka <file.um>\n");
+        printf("Usage: umka <file.um> [-storage <storage-size>] [-stack <stack-size>]\n");
         return 1;
     }
 
-    int res = umkaInit(argv[1], 1024 * 1024, argc, argv);
+    int storageSize = 1024 * 1024;  // Bytes
+    int stackSize   = 1024 * 1024;  // Slots
+
+    for (int i = 0; i < 4; i += 2)
+    {
+        if (argc > 2 + i)
+        {
+            if (strcmp(argv[2 + i], "-storage") == 0)
+            {
+                if (argc == 2 + i + 1)
+                {
+                    printf("Illegal command line parameter\n");
+                    return 1;
+                }
+
+                storageSize = strtol(argv[2 + i + 1], NULL, 0);
+                if (storageSize <= 0)
+                {
+                    printf("Illegal storage size\n");
+                    return 1;
+                }
+            }
+            else if (strcmp(argv[2 + i], "-stack") == 0)
+            {
+                if (argc == 2 + i + 1)
+                {
+                    printf("Illegal command line parameter\n");
+                    return 1;
+                }
+
+                stackSize = strtol(argv[2 + i + 1], NULL, 0);
+                if (stackSize <= 0)
+                {
+                    printf("Illegal stack size\n");
+                    return 1;
+                }
+            }
+        }
+    }
+
+    int res = umkaInit(argv[1], storageSize, argc, argv);
 
     if (res == 0)
     {
@@ -34,7 +75,7 @@ int main(int argc, char **argv)
 
     if (res == 0)
     {
-        res = umkaRun(1024 * 1024);
+        res = umkaRun(stackSize);
 
         if (res != 0)
         {
