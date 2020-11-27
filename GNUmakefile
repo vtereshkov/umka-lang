35723bb3019f38e00ee8e396671d6b6685108181
skaller <skaller@users.sourceforge.net>
2020-11-27 15:47:01 +1100

    Implement visibility control. Support MacOS.

---
+++ GNUmakefile
@@ -0,0 +1,58 @@
+platform := $(shell uname -s)
+ifeq ($(platform), Linux)
+  LPATH = LD_LIBRARY_PATH
+  STATIC_CFLAGS = -fPIC -O3 -Wall -Wno-format-security -DUMKA_STATIC
+  DYNAMIC_CFLAGS = -fPIC -O3 -Wall -fvisibility=hidden -DUMKA_BUILD
+  STATIC_LDFLAGS = -static-libgcc
+  DYNAMIC_LDFLAGS = -shared -lm
+  STATIC_LIB = libumka.a
+  DYNAMIC_LIB = libumka.so
+  RANLIB = ar -c
+else
+ifeq ($(platform), Darwin)
+  LPATH = DYLD_LIBRARY_PATH
+  STATIC_CFLAGS = -fPIC -O3 -Wall -Wno-format-security -DUMKA_STATIC
+  DYNAMIC_CFLAGS = -fPIC -O3 -Wall -fvisibility=hidden -DUMKA_BUILD
+  STATIC_LDFLAGS = 
+  DYNAMIC_LDFLAGS = -shared
+  STATIC_LIB = libumka.a
+  DYNAMIC_LIB = libumka.dylib
+  RANLIB = libtool -static 
+endif
+endif
+
+ifndef LPATH
+$(warning Unrecognized kernel name -- Unable to detect setting for LPATH)
+endif
+
+STATIC_LIB_OBJ = src/umka_api_static.o src/umka_common_static.o src/umka_compiler_static.o src/umka_const_static.o src/umka_decl_static.o src/umka_expr_static.o src/umka_gen_static.o src/umka_ident_static.o src/umka_lexer_static.o src/umka_runtime_static.o src/umka_stmt_static.o src/umka_types_static.o src/umka_vm_static.o
+DYNAMIC_LIB_OBJ = src/umka_api_dynamic.o src/umka_common_dynamic.o src/umka_compiler_dynamic.o src/umka_const_dynamic.o src/umka_decl_dynamic.o src/umka_expr_dynamic.o src/umka_gen_dynamic.o src/umka_ident_dynamic.o src/umka_lexer_dynamic.o src/umka_runtime_dynamic.o src/umka_stmt_dynamic.o src/umka_types_dynamic.o src/umka_vm_dynamic.o
+EXECUTABLE_OBJ = src/umka_static.o
+EXECUTABLE = umka
+
+.PHONY: all clean
+all: $(EXECUTABLE) $(STATIC_LIB) $(DYNAMIC_LIB)
+
+clean:
+	rm -f $(EXECUTABLE) $(STATIC_LIB) $(DYNAMIC_LIB) 
+	rm -f src/*.o
+
+$(STATIC_LIB): $(STATIC_LIB_OBJ)
+	# Build static archive 
+	$(RANLIB) $(STATIC_LDFLAGS) -o $(STATIC_LIB) $^
+
+$(DYNAMIC_LIB): $(DYNAMIC_LIB_OBJ)
+	# Build shared library
+	$(CC) $(DYNAMIC_LDFLAGS) -o $(DYNAMIC_LIB) $^
+
+$(EXECUTABLE): $(STATIC_LIB) $(EXECUTABLE_OBJ) 
+	# Build executable 
+	$(CC) $(STATIC_CFLAGS) -o $(EXECUTABLE) $^
+
+src/%_static.o: src/%.c
+	$(CC) $(STATIC_CFLAGS) -c -o $@ $^
+
+
+src/%_dynamic.o: src/%.c
+	$(CC) $(DYNAMIC_CFLAGS) -c -o $@ $^
+
--- src/umka_api.h
+++ src/umka_api.h
@@ -1,6 +1,28 @@
 #ifndef UMKA_API_H_INCLUDED
 #define UMKA_API_H_INCLUDED
 
+#ifdef _WIN32
+  #if defined(UMKA_STATIC)
+    #define UMKA_EXPORT
+    #define UMKA_IMPORT
+  #else
+    #define UMKA_EXPORT __declspec(dllexport)
+    #define UMKA_IMPORT __declspec(dllimport)
+  #endif
+#else
+  // All modules on Unix are compiled with -fvisibility=hidden
+  // All API symbols get visibility default
+  // whether or not we're static linking or dynamic linking (with -fPIC)
+  #define UMKA_EXPORT __attribute__((visibility("default"))) 
+  #define UMKA_IMPORT __attribute__((visibility("default"))) 
+#endif
+
+#ifdef UMKA_BUILD
+#define UMKA_EXTERN UMKA_EXPORT
+#else
+#define UMKA_EXTERN UMKA_IMPORT
+#endif
+
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -36,16 +58,16 @@ typedef struct
 } UmkaError;
 
 
-void *umkaAlloc     (void);
-bool umkaInit       (void *umka, const char *fileName, int storageSize, int stackSize, int argc, char **argv);
-bool umkaCompile    (void *umka);
-bool umkaRun        (void *umka);
-bool umkaCall       (void *umka, int entryOffset, int numParamSlots, UmkaStackSlot *params, UmkaStackSlot *result);
-void umkaFree       (void *umka);
-void umkaGetError   (void *umka, UmkaError *err);
-void umkaAsm        (void *umka, char *buf);
-void umkaAddFunc    (void *umka, const char *name, UmkaExternFunc entry);
-int  umkaGetFunc    (void *umka, const char *moduleName, const char *funcName);
+void UMKA_EXTERN *umkaAlloc     (void);
+bool UMKA_EXTERN umkaInit       (void *umka, const char *fileName, int storageSize, int stackSize, int argc, char **argv);
+bool UMKA_EXTERN umkaCompile    (void *umka);
+bool UMKA_EXTERN umkaRun        (void *umka);
+bool UMKA_EXTERN umkaCall       (void *umka, int entryOffset, int numParamSlots, UmkaStackSlot *params, UmkaStackSlot *result);
+void UMKA_EXTERN umkaFree       (void *umka);
+void UMKA_EXTERN umkaGetError   (void *umka, UmkaError *err);
+void UMKA_EXTERN umkaAsm        (void *umka, char *buf);
+void UMKA_EXTERN umkaAddFunc    (void *umka, const char *name, UmkaExternFunc entry);
+int  UMKA_EXTERN umkaGetFunc    (void *umka, const char *moduleName, const char *funcName);
 
 
 #if defined(__cplusplus)
