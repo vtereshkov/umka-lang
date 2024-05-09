7d03a8f8cfdd5743d51cff9a5c7fce2240a50832
Vasiliy Tereshkov <52492460+vtereshkov@users.noreply.github.com>
2024-05-10 03:07:48 +0400

    Relax weak ptr restrictions (#381, #382)

--- src/umka_expr.h
+++ src/umka_expr.h
@@ -8,9 +8,9 @@ void doPushConst                    (Compiler *comp, Type *type, Const *constant
 void doPushVarPtr                   (Compiler *comp, Ident *ident);
 void doCopyResultToTempVar          (Compiler *comp, Type *type);
 bool doTryRemoveCopyResultToTempVar (Compiler *comp);
-void doImplicitTypeConv             (Compiler *comp, Type *dest, Type **src, Const *constant, bool lhs);
-void doAssertImplicitTypeConv       (Compiler *comp, Type *dest, Type **src, Const *constant, bool lhs);
-void doExplicitTypeConv             (Compiler *comp, Type *dest, Type **src, Const *constant, bool lhs);
+void doImplicitTypeConv             (Compiler *comp, Type *dest, Type **src, Const *constant);
+void doAssertImplicitTypeConv       (Compiler *comp, Type *dest, Type **src, Const *constant);
+void doExplicitTypeConv             (Compiler *comp, Type *dest, Type **src, Const *constant);
 void doApplyOperator                (Compiler *comp, Type **type, Type **rightType, Const *constant, Const *rightConstant, TokenKind op, bool apply, bool convertLhs);
 
 Ident *parseQualIdent               (Compiler *comp);
