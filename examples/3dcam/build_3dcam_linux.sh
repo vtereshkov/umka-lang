158556caf61e7dc2abb3910c95f0c6e083685a8c
Lucas Wagner <lowagner@gmail.com>
2025-11-26 05:39:45 -0800

    fix 3dcam build script for linux

--- examples/3dcam/build_3dcam_linux.sh
+++ examples/3dcam/build_3dcam_linux.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-cp ../../libumka.so .
 cp ../../umka_api.h .
+cp ../../libumka.so .
+if [ $? -ne 0 ]; then
+    echo "Use build_linux.sh in the root umka directory,"
+    echo "then run the example in the created umka_linux directory."
+    return 1
+fi
 
-gcc 3dcam.c -o 3dcam -lumka -lraylib -L$PWD -Wl,-rpath,'$ORIGIN'
+gcc 3dcam.c -o 3dcam -lumka -lraylib -L$PWD -lm -Wl,-rpath,'$ORIGIN'
